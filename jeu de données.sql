d0b7e5c313f08fd5a6a6458bfa418cdb22adf3c4
Baptiste DEMARCHE <batmine3@gmail.com>
2020-03-29 23:24:23 +0200

    Create file to import data

---
+++ jeu de données.sql
@@ -0,0 +1,28 @@
+INSERT INTO TRUCK (truckManufacturers, truckModel, licensePlate, km) VALUES ('Mercedes', 'New Actros','AA-777-AA', 12);
+INSERT INTO TRUCK (truckManufacturers, truckModel, licensePlate, km) VALUES ('Renault', 'Proof','BB-888-BB', 500);
+INSERT INTO TRUCK (truckManufacturers, truckModel, licensePlate, km) VALUES ('Ford', 'Mustang','CC-111-CC', 12);
+INSERT INTO TRUCK (truckManufacturers, truckModel, licensePlate, km) VALUES ('Volvo', 'Bullet','775 EBJ 75', 12);
+
+INSERT INTO OPENDAYS(openDay, startHour, endHour, truck) VALUES ('Lundi', '08:00', '17:00', 1);
+INSERT INTO OPENDAYS(openDay, startHour, endHour, truck) VALUES ('Mardi', '08:00', '17:00', 1);
+INSERT INTO OPENDAYS(openDay, startHour, endHour, truck) VALUES ('Mercredi', '08:00', '17:00', 1);
+INSERT INTO OPENDAYS(openDay, startHour, endHour, truck) VALUES ('Jeudi', '08:00', '17:00', 1);
+INSERT INTO OPENDAYS(openDay, startHour, endHour, truck) VALUES ('Vendredi', '08:00', '17:00', 1);
+
+INSERT INTO OPENDAYS(openDay, startHour, endHour, truck) VALUES ('Samedi', '08:00', '17:00', 2);
+INSERT INTO OPENDAYS(openDay, startHour, endHour, truck) VALUES ('Mardi', '08:00', '17:00', 2);
+INSERT INTO OPENDAYS(openDay, startHour, endHour, truck) VALUES ('Mercredi', '08:00', '17:00', 2);
+INSERT INTO OPENDAYS(openDay, startHour, endHour, truck) VALUES ('Jeudi', '08:00', '17:00', 2);
+INSERT INTO OPENDAYS(openDay, startHour, endHour, truck) VALUES ('Vendredi', '08:00', '17:00', 2);
+
+INSERT INTO OPENDAYS(openDay, startHour, endHour, truck) VALUES ('Samedi', '08:00', '17:00', 3);
+INSERT INTO OPENDAYS(openDay, startHour, endHour, truck) VALUES ('Dimanche', '08:00', '17:00', 3);
+INSERT INTO OPENDAYS(openDay, startHour, endHour, truck) VALUES ('Mercredi', '08:00', '17:00', 3);
+INSERT INTO OPENDAYS(openDay, startHour, endHour, truck) VALUES ('Jeudi', '08:00', '17:00', 3);
+INSERT INTO OPENDAYS(openDay, startHour, endHour, truck) VALUES ('Vendredi', '08:00', '17:00', 3);
+
+INSERT INTO OPENDAYS(openDay, startHour, endHour, truck) VALUES ('Lundi', '08:00', '17:00', 4);
+INSERT INTO OPENDAYS(openDay, startHour, endHour, truck) VALUES ('Dimanche', '08:00', '17:00', 4);
+INSERT INTO OPENDAYS(openDay, startHour, endHour, truck) VALUES ('Mercredi', '08:00', '17:00', 4);
+INSERT INTO OPENDAYS(openDay, startHour, endHour, truck) VALUES ('Samedi', '08:00', '17:00', 4);
+INSERT INTO OPENDAYS(openDay, startHour, endHour, truck) VALUES ('Vendredi', '08:00', '17:00', 4);
