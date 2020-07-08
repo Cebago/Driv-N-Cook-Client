90535a7f0277ea75a87c6b1d3770132afd7a1741
Batmine3 <batmine3@gmail.com>
2020-07-09 00:36:54 +0200

    other possibiliy

--- eventsDetails.php
+++ eventsDetails.php
@@ -1,56 +1,66 @@
 <?php
-require 'navbar.php';
-
-if($_GET["idEvent"] != ""){
-    $pdo = connectDB();
-    $queryPrepared = $pdo->prepare("SELECT * FROM EVENTS WHERE idEvent = :idEvent;");
-    $queryPrepared->execute([
-        ":idEvent" => $_GET["idEvent"]
-    ]);
-    $events = $queryPrepared->fetchAll(PDO::FETCH_ASSOC);
-
+if(!isset($_GET) || empty($_GET)){
+    header("Location: events.php");
 }
+require 'functions.php';
+
+$pdo = connectDB();
+$queryPrepared = $pdo->prepare("SELECT idEvent, eventType, eventName, eventDesc, eventAddress, eventCity, eventPostalCode, 
+       DATE_FORMAT(eventBeginDate, '%d/%m/%Y') as eventBeginDate, DATE_FORMAT(eventEndDate, '%d/%m/%Y') as eventEndDate, 
+       DATE_FORMAT(eventStartHour, '%H:%i') as eventStartHour, DATE_FORMAT(eventEndHour, '%H:%i') as eventEndHour, eventImg FROM EVENTS WHERE idEvent = :id AND eventType = 'Dégustation'");
+$queryPrepared->execute([":id" => $_GET["idEvent"]]);
+$infos = $queryPrepared->fetch(PDO::FETCH_ASSOC);
+if (empty($infos)) {
+    header("Location: events.php");
+}
+require "navbar.php";
 ?>
 <!-- Banner Area Starts -->
 <section class="banner-area banner-area2 contact-bg text-center">
     <div class="container">
         <div class="row">
             <div class="col-lg-12">
-                <h1><i><?php foreach ($events as $name){ echo $name["eventName"]; }?></i></h1>
-
+                <h1>
+                    <i>
+                        <?php echo $infos["eventName"]; ?>
+                    </i>
+                </h1>
             </div>
         </div>
     </div>
 </section>
-<!-- Banner Area End -->
 
-<!-- Food Area starts -->
 <section class="update-area section-padding">
     <div class="container">
         <div class="row">
-            <?php foreach ($events as $event){?>
-            <div class="food-content">
-                <h5><?php echo $event["eventDesc"]; ?> </h5>
-
-                <div class="card mb-3 mt-5" style="max-width: 540px;">
-                    <div class="row no-gutters">
-                        <div class="single-slide d-sm-flex" style="height: 200px;">
-                            <div class="customer-img mr-4 mb-4 mb-sm-0">
-                                <img src="<?php echo $event["eventImg"] ?>" alt="" class="mb-4">
-                            </div>
-                        <div class="col-md-8">
-                            <div class="card-body">
-                                <h5 class="card-title"><?php echo $event["eventType"]; ?></h5>
-                                <p><i class="fa fa-map-marker" aria-hidden="true"></i>&nbsp<?php echo $event["eventAddress"] .", ". $event["eventPostalCode"]." ". $event["eventCity"]; ?></p>
-                                <p class="card-text"><?php echo $event["eventBeginDate"] ."&nbsp=>&nbsp". $event["eventEndDate"];?></p>
-                                <p class="card-text"><small class="text-muted"><?php echo $event["eventStartHour"] ." / ". $event["eventEndHour"];?></small></p>
-                            </div>
-                        </div>
+            <div class="col-6">
+                <img src="<?php echo $infos["eventImg"] ?>" alt="" class="img-fluid">
+            </div>
+            <div class="col-2"></div>
+            <div class="col-4">
+                <div class="card">
+                    <div class="card-header">
+                        <?php echo $infos["eventDesc"]; ?>
+                    </div>
+                    <div class="card-body">
+                        <p>
+                            <?php
+                            echo getTranslate("Ouvert du", $tabLang, $setLanguage) . " " . $infos["eventBeginDate"] . " - " . $infos["eventStartHour"];
+                            ?>
+                        </p>
+                        <p>
+                            <?php
+                            echo getTranslate("au", $tabLang, $setLanguage) . " " . $infos["eventBeginDate"] . " - " . $infos["eventStartHour"];
+                            ?>
+                        </p>
+                    </div>
+                    <div class="card-footer">
+                        <?php
+                        echo $infos["eventAddress"] . ", " . $infos["eventPostalCode"] . "&nbsp;" . $infos["eventCity"];
+                        ?>
                     </div>
                 </div>
-
             </div>
-    <?php } ?>
         </div>
     </div>
 </section>
